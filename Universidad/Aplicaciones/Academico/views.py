e33c827a34b2718bf35acc16a76e80f3fe9d07ef
Nahuel Silva <nahuel_11_arg@hotmail.com>
2021-09-17 00:51:15 -0300

    diseño

---
+++ Universidad/Aplicaciones/Academico/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Curso
+
+def home(request):
+    cursosListados = Curso.objects.all()
+    return render(request, "gestionCursos.html", {"cursos":cursosListados})
